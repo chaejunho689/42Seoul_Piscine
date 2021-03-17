45ed66fd19dd21159f608c49e0bf9b5ab25c9775
chaejunho689 <chaejunho689@gmail.com>
2021-03-17 19:46:45 +0900

    432

---
+++ table/C02TEST/ex10/ft_strlcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchae <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/16 00:39:06 by jchae             #+#    #+#             */
+/*   Updated: 2021/03/16 00:39:35 by jchae            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int result;
+
+	i = 0;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	result = j;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (result);
+}
